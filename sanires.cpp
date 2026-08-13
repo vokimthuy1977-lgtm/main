354ee666204e79f471131ba2426d55641a1e790b
vokimthuy1977-lgtm <vokimthuy1977@gmail.com>
2026-08-13 12:52:28 +0700

    Beta.16

--- sanires.cpp
+++ sanires.cpp
@@ -61,8 +61,22 @@ void scanFileContent(const std::filesystem::path* _dir_path, std::vector<std::st
 		if (_element.operator*().is_directory() == true) {
 			scanFileContent(&(_element.operator*().path()), pArrayNameDollar);
 		} else if (_element.operator*().is_regular_file() == true) {
-			//std::filesystem::path 
-			//std::filesystem::path parentFolderContainFile = _element.operator*().path().parent_path()
+			std::filesystem::path r = _element.operator*().path();
+			std::string fw = r.replace_extension("").string() + ".tmp";
+			std::ofstream file_write(fw);
+			std::ifstream file_read(_element.operator*().path());
+			std::string line;
+			while (getline(file_read, line)) {
+				for (std::string nameDollar : *pArrayNameDollar) {
+					std::string nameFix = nameDollar;
+					nameFix[0] = '_';
+					replaceString(line, nameDollar, nameFix);
+				}
+				file_write << line << "\n";
+			}
+			std::filesystem::path fileOld(fw)
+			std::filesystem::remove(r);
+			std::filesystem::rename(fileOld, r);
 		}
 		_element.operator++();
 	}
